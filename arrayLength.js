fa90b4933211b9f009f87f3c2f14d61753e56e5b
Joshua Ow <joshuaow@gmail.com>
2023-07-08 17:26:51 -0500

    leet code

---
+++ arrayLength.js
@@ -0,0 +1,9 @@
+//  Leetcode - easy
+
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+// Solution
+
+var argumentsLength = function(...args) {
+    return args.length
+};

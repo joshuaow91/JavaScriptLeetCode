1a56f8eca1e854d780fde76eb28393129b1aa9ab
Joshua Ow <joshuaow@gmail.com>
2023-07-08 17:36:52 -0500

    leet code

---
+++ isObjectEmpty.js
@@ -0,0 +1,18 @@
+//  leet code - easy
+
+// Given an object or an array, return if it is empty.
+// An empty object contains no key-value pairs.
+// An empty array contains no elements.
+// You may assume the object or array is the output of JSON.parse.
+
+//  Solution
+
+function isEmpty(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else if (typeof obj === 'object' && obj !== null) {
+        return Object.keys(obj).length === 0;
+    } else {
+        throw new TypeError("Expected object or array");
+    }
+}

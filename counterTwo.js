600c3cd77870d1e574af2a1d57689ed006f75499
Joshua Ow <joshuaow@gmail.com>
2023-07-18 08:31:33 -0500

    leetcode

---
+++ counterTwo.js
@@ -0,0 +1,26 @@
+// leetcode easy
+
+// Write a function createCounter. It should accept an initial integer init. It should return an object with three functions.
+
+// The three functions are:
+
+// increment() increases the current value by 1 and then returns it.
+// decrement() reduces the current value by 1 and then returns it.
+// reset() sets the current value to init and then returns it.
+
+// Solution
+
+var createCounter = function(init) {
+    let count = init
+    return {
+        increment () {
+            return ++count
+        },
+        decrement () {
+             return --count
+        },
+        reset () {
+            return count = init
+        }
+    }
+};

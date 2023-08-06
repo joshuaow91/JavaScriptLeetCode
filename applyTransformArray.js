6a09dd9621000cf0bc728c2f58841636c7ddc552
Joshua Ow <joshuaow@gmail.com>
2023-08-06 14:01:54 -0500

    leetcode

---
+++ applyTransformArray.js
@@ -0,0 +1,17 @@
+//  leetcode easy
+
+// Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+// The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+// Please solve it without the built-in Array.map method.
+
+// solution
+
+var map = function(arr, fn) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i));
+    }
+    return result;
+};
